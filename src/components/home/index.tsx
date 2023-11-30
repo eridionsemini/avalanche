6467f27fbceaff64c5d4b97ec4ee966069be3a31
eridion semini <eridionsemini@gmail.com>
2023-11-30 17:21:50 +0100

    initial setup

---
+++ src/components/home/index.tsx
@@ -0,0 +1,10 @@
+import React, {FC, ReactElement} from "react";
+import {HomeProps} from "./types";
+
+export const Home: FC<HomeProps> = ({title}): ReactElement => {
+    return (
+        <div>
+            {title}
+        </div>
+    )
+}
