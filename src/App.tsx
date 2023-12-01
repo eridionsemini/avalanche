161f21d992f97006678f0f1825d89729f00e88db
eridion semini <eridionsemini@gmail.com>
2023-12-01 19:20:04 +0100

    Apollo Provider Configuration env variables configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
 import React from 'react';
+import {InMemoryCache, ApolloClient, ApolloProvider} from "@apollo/client";
+
 import logo from './logo.svg';
 import './App.css';
 
+const cache = new InMemoryCache({
+    typePolicies: {
+        Query: {
+            fields: {
+                clients: {
+                    merge(existing, incoming) {
+                        return incoming;
+                    },
+                },
+                projects: {
+                    merge(existing, incoming) {
+                        return incoming;
+                    },
+                },
+            },
+        },
+    },
+});
+
+
+const client = new ApolloClient({
+    uri: process.env.REACT_APP_GRAPHQL_API,
+    cache
+})
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <ApolloProvider client={client}>
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo"/>
+                    <p>
+                        Edit <code>src/App.tsx</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        </ApolloProvider>
+    );
 }
 
 export default App;
