067d765f22f1d0354bb9101f37df3434fbe6b428
eridion semini <eridionsemini@gmail.com>
2023-12-01 21:53:19 +0100

    connect Apollo with WebSocket

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
-import {InMemoryCache, ApolloClient, ApolloProvider} from "@apollo/client";
+import {InMemoryCache, ApolloClient, ApolloProvider, split, HttpLink} from "@apollo/client";
+import {getMainDefinition} from '@apollo/client/utilities';
+import {GraphQLWsLink} from '@apollo/client/link/subscriptions';
+import {createClient} from 'graphql-ws';
+
 
 import logo from './logo.svg';
 import './App.css';
+
+const fallbackWebSocketUrl = 'ws://ec2-3-216-28-214.compute-1.amazonaws.com:4000';
 
 const cache = new InMemoryCache({
     typePolicies: {
@@ -18,10 +24,31 @@ const cache = new InMemoryCache({
     },
 });
 
+const httpLink = new HttpLink({
+    uri: process.env.REACT_APP_GRAPHQL_API_URL
+});
+
+const wsLink = new GraphQLWsLink(createClient({
+    url: process.env.REACT_APP_WEB_SOCKET_URL ?? fallbackWebSocketUrl
+}));
+
+const splitLink = split(
+    ({query}) => {
+        const definition = getMainDefinition(query);
+        return (
+            definition.kind === 'OperationDefinition' &&
+            definition.operation === 'subscription'
+        );
+    },
+    wsLink,
+    httpLink,
+);
+
 
 const client = new ApolloClient({
-    uri: process.env.REACT_APP_GRAPHQL_API,
-    cache
+    link: splitLink,
+    cache,
+    connectToDevTools: process.env.NODE_ENV === 'development'
 })
 
 function App() {
